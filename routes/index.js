453a83405cdd4eeb68be398c9a25ccdbf8a1c4fd
Aaron Biliyok <abiliyok@gmail.com>
2017-07-05 21:22:15 +0100

    refactored boilerplate codes to ES6

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
+/* GET login page. */
 router.get('/', function(req, res, next) {
   res.render('login', { title: 'PostIt Messaging App',login:'login' });
 });
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', { register:'Register' });
+});
+
+/* GET register page. */
+router.get('/password/reset', function(req, res, next) {
+  res.render('email_reset', { reset:'Reset Password' });
+});
+
 
 module.exports = router;
