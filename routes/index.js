b6199d5437d76e043929d81a47b2ee8c5e14c816
Aaron Biliyok <abiliyok@gmail.com>
2017-07-08 15:08:35 +0100

    refactored the index.js file to ES6 syntax

--- routes/index.js
+++ routes/index.js
@@ -2,42 +2,45 @@ const express = require('express');
 const router = express.Router();
 
 /* GET welcome page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next)=> {
   res.render('index', { title: 'PostIt Messaging App',login:'login' });
 });
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {
+router.get('/login', (req, res, next)=> {
   res.render('login', { title: 'PostIt Messaging App',login:'login' });
 });
 
 
 /* GET register page. */
-router.get('/register', function(req, res, next) {
+router.get('/register', (req, res, next)=> {
   res.render('register', { register:'Register' });
 });
 
 /* GET register page. */
-router.get('/password/reset', function(req, res, next) {
+router.get('/password/reset', (req, res, next)=> {
   res.render('email_reset', { reset:'Reset Password' });
 });
 
 /*route to create new group*/
-router.get('/new/group', function(req, res, next) {
+router.get('/new/group', (req, res, next)=> {
   res.render('new_group', { group:'Create New Group' });
 });
 
-router.get('/groups', function(req, res, next) {
+router.get('/groups', (req, res, next)=> {
   res.render('message_board');
 });
 
-router.get('/new/message', function(req, res, next) {
+router.get('/new/message', (req, res, next)=> {
   res.render('new_message');
 });
 
-router.get('/home', function(req, res, next) {
+router.get('/home', (req, res, next)=> {
   res.render('index');
 });
 
+/*router.get('/api/group/:id/messages', function(req, res, next) {
+  res.send(req.params);
+});*/
 
 module.exports = router;
