4b4ec4f6c6f5999d48acf1c3b60a0679d64c61d5
Aaron Biliyok <abiliyok@gmail.com>
2017-07-27 16:53:20 +0100

    Initial setup for react development environment. Setting up webpack

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+/*eslint-disable */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component{
+     render(){
+         return (
+           <h2>Welcome to PostIt</h2>
+         );
+     }
+}
+
+export default App;
